2d00b6da1842185bdb7d292fd375b7b52d84dbd4
ThieuQuangHuy23021580 <160751958+ThieuQuangHuy23021580@users.noreply.github.com>
2024-11-12 03:09:56 +0700

    NewMainView_fix5

--- src/main/java/controller/libraryapp/MainViewController.java
+++ src/main/java/controller/libraryapp/MainViewController.java
@@ -1,4 +1,81 @@
 package controller.libraryapp;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class MainViewController {
+
+    @FXML
+    private AnchorPane bookPane;
+
+    @FXML
+    private Button categoriesButton_active;
+
+    @FXML
+    private Button categoriesButton_inactive;
+
+    @FXML
+    private Label discover;
+
+    @FXML
+    private FlowPane flowHome;
+
+    @FXML
+    private Rectangle gradient_down_rectangle;
+
+    @FXML
+    private Rectangle gradient_left_rectangle;
+
+    @FXML
+    private Rectangle gradient_right_rectangle;
+
+    @FXML
+    private Rectangle gradient_up_rectangle;
+
+    @FXML
+    private Button homeButton_active;
+
+    @FXML
+    private Button homeButton_inactive;
+
+    @FXML
+    private AnchorPane home_anchorpane;
+
+    @FXML
+    private Label librahub;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private Button the_most_popular_books;
+
+    @FXML
+    private MenuButton userMenuButton;
+
+    @FXML
+    private Label userName;
+
+
+    @FXML
+    void initialize() throws IOException {
+        StackPane bookObject = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("controller/fxml_designs/BookObject.fxml")));
+        bookPane.getChildren().add(bookObject);
+    }
+
 }
