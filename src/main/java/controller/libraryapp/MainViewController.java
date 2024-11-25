9e737c13121983b8db0347ffe710d73b7f67109b
ThieuQuangHuy23021580 <160751958+ThieuQuangHuy23021580@users.noreply.github.com>
2024-11-25 21:58:26 +0700

    UserInfo_fix

--- src/main/java/controller/libraryapp/MainViewController.java
+++ src/main/java/controller/libraryapp/MainViewController.java
@@ -20,10 +20,19 @@ import java.util.Objects;
 public class MainViewController {
 
     @FXML
-    private FlowPane recommendFlowPane;
+    private TextField authorConfigTextField;
 
     @FXML
-    private AnchorPane bookPane;
+    private AnchorPane bookObjectInfo;
+
+    @FXML
+    private Button businessAndEconomicButton;
+
+    @FXML
+    private AnchorPane businessAndEconomicCateList;
+
+    @FXML
+    private AnchorPane categoriesAnchorPane;
 
     @FXML
     private Button categoriesButton_active;
@@ -32,7 +41,43 @@ public class MainViewController {
     private Button categoriesButton_inactive;
 
     @FXML
+    private Button comicButton;
+
+    @FXML
+    private AnchorPane comicCateList;
+
+    @FXML
+    private Button configAuthor;
+
+    @FXML
+    private Button configAuthor1;
+
+    @FXML
+    private Button configAuthor11;
+
+    @FXML
+    private Button configAuthor111;
+
+    @FXML
+    private TextField descriptionConfigTextField;
+
+    @FXML
     private Label discover;
+
+    @FXML
+    private AnchorPane discoverCatetegories;
+
+    @FXML
+    private Button educationButton;
+
+    @FXML
+    private AnchorPane educationCateList;
+
+    @FXML
+    private Button fictionButton;
+
+    @FXML
+    private AnchorPane fictionCateList;
 
     @FXML
     private FlowPane flowHome;
@@ -50,16 +95,40 @@ public class MainViewController {
     private Rectangle gradient_up_rectangle;
 
     @FXML
+    private Button healthAndFitnessButton;
+
+    @FXML
+    private AnchorPane healthAndFitnessCateList;
+
+    @FXML
+    private AnchorPane homeAnchorPane;
+
+    @FXML
     private Button homeButton_active;
 
     @FXML
     private Button homeButton_inactive;
 
     @FXML
-    private AnchorPane home_anchorpane;
+    private Label librahub;
 
     @FXML
-    private Label librahub;
+    private AnchorPane otherCateList;
+
+    @FXML
+    private Button otherCategoriesButton;
+
+    @FXML
+    private TextField publishConfigTextField;
+
+    @FXML
+    private TextField quantityConfigTextField;
+
+    @FXML
+    private AnchorPane recommendAnchorPane;
+
+    @FXML
+    private FlowPane recommendFlowPane;
 
     @FXML
     private Button searchButton;
@@ -71,6 +140,9 @@ public class MainViewController {
     private Button the_most_popular_books;
 
     @FXML
+    private Label tiltleBook;
+
+    @FXML
     private MenuButton userMenuButton;
 
     @FXML
@@ -79,7 +151,54 @@ public class MainViewController {
     @FXML
     public void initialize(){
     showBook();
+    fictionButton.setOnMouseEntered(event-> getFictionCateList());
+    fictionButton.setOnMouseExited(event-> getDiscoveryCategories());
+    educationButton.setOnMouseEntered(event-> getEducationCateList());
+    educationButton.setOnMouseExited(event-> getDiscoveryCategories());
+    comicButton.setOnMouseEntered(event-> getComicCateList());
+    comicButton.setOnMouseExited(event-> getDiscoveryCategories());
+    healthAndFitnessButton.setOnMouseEntered(event-> getHealthAndFitnessCateList());
+    healthAndFitnessButton.setOnMouseExited(event-> getDiscoveryCategories());
+    businessAndEconomicButton.setOnMouseEntered(event-> getBusinessAndEconomicCateList());
+    businessAndEconomicButton.setOnMouseExited(event-> getDiscoveryCategories());
+    otherCategoriesButton.setOnMouseEntered(event-> getOtherCateList());
+    otherCategoriesButton.setOnMouseExited(event-> getDiscoveryCategories());
     }
+
+    void getDiscoveryCategories(){
+        discoverCatetegories.setVisible(true);
+        fictionCateList.setVisible(false);
+        educationCateList.setVisible(false);
+        otherCateList.setVisible(false);
+        comicCateList.setVisible(false);
+        healthAndFitnessCateList.setVisible(false);
+        businessAndEconomicCateList.setVisible(false);
+    }
+    void getFictionCateList(){
+        fictionCateList.setVisible(true);
+        discoverCatetegories.setVisible(false);
+    }
+    void getEducationCateList(){
+        educationCateList.setVisible(true);
+        discoverCatetegories.setVisible(false);
+    }
+    void getComicCateList(){
+        comicCateList.setVisible(true);
+        discoverCatetegories.setVisible(false);
+    }
+    void getHealthAndFitnessCateList(){
+        healthAndFitnessCateList.setVisible(true);
+        discoverCatetegories.setVisible(false);
+    }
+    void getBusinessAndEconomicCateList(){
+        businessAndEconomicCateList.setVisible(true);
+        discoverCatetegories.setVisible(false);
+    }
+    void getOtherCateList(){
+        otherCateList.setVisible(true);
+        discoverCatetegories.setVisible(false);
+    }
+
     // Tạo đối tượng là sách.
     void showBook(){
         try {
