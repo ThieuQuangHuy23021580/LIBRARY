095aa9864864839f983202a635c63c74d0ee896d
ThieuQuangHuy23021580 <160751958+ThieuQuangHuy23021580@users.noreply.github.com>
2024-11-26 20:43:23 +0700

    split_BookObjectInfo

--- src/main/java/controller/libraryapp/MainViewController.java
+++ src/main/java/controller/libraryapp/MainViewController.java
@@ -219,5 +219,15 @@ public class MainViewController {
     }
 
 
+    public void configButtonPress(ActionEvent actionEvent) {
+    }
 
+    public void deleteButtonPress(ActionEvent actionEvent) {
+    }
+
+    public void borrowButtonPress(ActionEvent actionEvent) {
+    }
+
+    public void returnButtonPress(ActionEvent actionEvent) {
+    }
 }
