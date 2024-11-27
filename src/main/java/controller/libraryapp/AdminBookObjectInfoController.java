997a318bcd46be90168b633cbecd6955078e4fb9
ThieuQuangHuy23021580 <160751958+ThieuQuangHuy23021580@users.noreply.github.com>
2024-11-27 09:06:20 +0700

    BookObjectInfo_fix

--- src/main/java/controller/libraryapp/AdminBookObjectInfoController.java
+++ src/main/java/controller/libraryapp/AdminBookObjectInfoController.java
@@ -1,4 +1,20 @@
 package controller.libraryapp;
 
+import javafx.event.ActionEvent;
+
 public class AdminBookObjectInfoController {
+    public void configButtonPress(ActionEvent actionEvent) {
+    }
+
+    public void deleteButtonPress(ActionEvent actionEvent) {
+    }
+
+    public void borrowButtonPress(ActionEvent actionEvent) {
+    }
+
+    public void returnButtonPress(ActionEvent actionEvent) {
+    }
+
+    public void addBookButtonPress(ActionEvent actionEvent) {
+    }
 }
